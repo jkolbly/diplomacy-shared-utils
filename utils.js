f882553a668ce8c5e89246e7fec014a10cb2fd00
Jason Kolbly <jason@rscheme.org>
2022-09-19 23:30:40 -0500

    Change documentation of retreats class

--- utils.js
+++ utils.js
@@ -154,7 +154,7 @@ const orderResultEnum = {
  * @property {Object.<string, Country>} nations
  * @property {Object.<string, Adjustment>} adjustments Disband/build orders for the creating/disbanding phase
  * @property {Object.<string, Object.<string, Order>>} orders Keys are nation id's. Values are objects mapping provinces to selected orders
- * @property {Object.<string, Object.<string, RetreatOrder>>} retreats Retreat orders for the retreat phase. Maps nation id's to list of retreat orders
+ * @property {Object.<string, Object.<string, RetreatOrder>>} retreats Retreat orders for the retreat phase. Values map provinces to retreats
  * @property {Object.<string, Dislodgement>} dislodgements Dislodgements caused by adjudicating this turn's orders. Not present if orders have not been adjudicated. Maps province id's to dislodgements
  */
 
